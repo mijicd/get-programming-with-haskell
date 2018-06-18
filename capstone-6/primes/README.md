b79e56279bc26b580e59f304d1dabc44cf7f6f17
Dejan Mijic <dejan.mjc@gmail.com>
2018-06-18 18:48:55 +0200

    Complete capstone 6

---
+++ capstone-6/primes/README.md
@@ -0,0 +1,6 @@
+# Working with Stack
+
+This chapter illustrates the usage of [Stack][1] for creating, building, testing
+and executing Haskell projects.
+
+[1]: https://docs.haskellstack.org/en/stable/README/
---
+++ capstone-6/primes/src/Primes.hs
@@ -0,0 +1,31 @@
+module Primes where
+
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (next:rest) = next : sieve noFactors
+  where noFactors = filter (not . (== 0) . (`mod` next)) rest
+
+primes :: [Int]
+primes = sieve [2 .. 10000]
+
+isPrime :: Int -> Maybe Bool
+isPrime n
+  | n < 2 = Nothing
+  | n >= length primes = Nothing
+  | otherwise = Just (n `elem` primes)
+
+unsafePrimeFactors :: Int -> [Int] -> [Int]
+unsafePrimeFactors 0 [] = []
+unsafePrimeFactors n [] = []
+unsafePrimeFactors n (next:primes) =
+  if n `mod` next == 0
+    then next : unsafePrimeFactors (n `div` next) (next : primes)
+    else unsafePrimeFactors n primes
+
+primeFactors :: Int -> Maybe [Int]
+primeFactors n
+  | n < 2 = Nothing
+  | n >= length primes = Nothing
+  | otherwise = Just (unsafePrimeFactors n primesLessThanN)
+  where
+    primesLessThanN = filter (<= n) primes
